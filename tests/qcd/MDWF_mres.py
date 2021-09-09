756a7d2fd8f41bf234aef2a4d570bb29bf23ec03
Tristan Ueding <trisueding@web.de>
2021-09-09 11:04:15 +0200

    Program to reproduce an error

--- tests/qcd/MDWF_mres.py
+++ tests/qcd/MDWF_mres.py
@@ -7,7 +7,7 @@ grid = g.grid([8,8,8,8], g.double)
 #rng = g.random("seed text")
 
 # Random gauge field
-U = g.qcd.gauge.unit(grid, rng)
+U = g.qcd.gauge.unit(grid)
 
 # Mobius domain-wall fermion
 fermion = g.qcd.fermion.mobius(U, mass=0.1, M5=1.8, b=1.5, c=0.5, Ls=12,
@@ -18,30 +18,28 @@ inv = g.algorithms.inverter
 pc = g.qcd.fermion.preconditioner
 
 # Even-odd-preconditioned CG solver
-slv_5d = inv.preconditioned(pc.eo2_ne(), inv.cg(eps = 1e-4, maxiter = 1000))
+slv_5d = inv.preconditioned(pc.eo2_ne(), inv.cg(eps = 1e-8, maxiter = 1000))
 
+# 5D propagator
 D5_inv=slv_5d(fermion)
-
-# Abstract fermion propagator using this solver
-
-#fermion_propagator = fermion.propagator(slv_5d)
 
 # Create 4D point source
 src4D = g.mspincolor(U[0].grid)
 g.create.point(src4D, [0, 0, 0, 0])
 
+# Short-cuts
 imp = fermion.ImportPhysicalFermionSource
 exp = fermion.ExportPhysicalFermionSolution
 
 # get 5D source from 4D source
-src5D =  imp * src4D
+src5D =  g(imp * src4D)
 
 # Solve propagator on 12 spin-color components
 
-#g.message("Calculate 5D propagator")
-#prop5D = g( D5_inv * src5D )
-#g.message("Extract 4D propagator")
-#prop4D = g( exp * prop5D )
+g.message("Calculate 5D propagator")
+prop5D = g( D5_inv * src5D )
+g.message("Extract 4D propagator")
+prop4D = g( exp * prop5D )
 #
 #g.message(prop5D)
 #g.message(prop4D)
